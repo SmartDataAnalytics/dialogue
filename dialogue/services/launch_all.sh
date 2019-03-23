c3a8c71c13061ab114ab9d3b85bd55c3bacbecca
lukovnikov <lukovnikov@outlook.com>
2019-03-23 23:45:30 +0100

    interactive coref tester

--- dialogue/services/launch_all.sh
+++ dialogue/services/launch_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python ../../ParZu/parzu_server.py -p 6500 &
-python ../../CorZu/server.py -p 6501 -q 6500 &
+python ../../ParZu/parzu_server.py -p 6500 -H 0.0.0.0 &
+python ../../CorZu/server.py -p 6501 -q 6500 -H 0.0.0.0 &
 python coref.py -p 8008 -s $R_MODELSIZE
